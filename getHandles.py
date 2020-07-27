c4b09393834939e0d2a3b62507d2e155521f4c34
David Vannen <david@vannen.co.uk>
2020-07-27 17:15:25 +0100

    Fork from digitalPlayer1125/twitterUsernamefromUserID
    Rebuilt code as callable module
    Pass list of user IDs
    Returns dictionary of user IDs -> usernames
    
    Added comments
    
    Line 15: Changed chrome_options to options due to deprecation
    Line 17: Path to Chromedriver must be absolute otherwise triggers permissions exception. Todo - make relative path work
    
     Changes to be committed:
    	modified:   getHandles.py

--- getHandles.py
+++ getHandles.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import json
 import time
 from selenium import webdriver
@@ -5,28 +7,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-ChromeOptions = webdriver.ChromeOptions()
-ChromeOptions.add_argument("--start-maximized")
-browser = webdriver.Chrome('./chromedriver', chrome_options=ChromeOptions)
-with open('./listIDS.txt') as f:
-    content = f.readlines()
-content = [x.strip() for x in content]  #Array of all id's
-data = {}
-data["people"]= []
-for i in content:
-    browser.get("https://twitter.com/i/user/"+i)
-    delay = 15
-    try:
-        myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'react-root')))
-        print ("Page is ready for -> ", i)
-        time.sleep(5)
-        print(browser.current_url)
-        endingslash= (str(browser.current_url).split('/'))[-1]
-        if not endingslash==i:
-            data['people'].append({
-                i: endingslash
-            })
-    except TimeoutException:
-        print ("Loading took too much time!")
-with open('twitterHandles.txt', 'w') as outfile:
-    json.dump(data, outfile)
+
+def getHandles(user_IDs = None):
+    ChromeOptions = webdriver.ChromeOptions()
+    # Start headless so no window popping up
+    ChromeOptions.add_argument("--headless")
+    # Changed chrome_options to options due to deprecation
+    # MUST be full path to Chromedriver NOT relative. Also must NOT have exe extension here
+    browser = webdriver.Chrome('/Users/davidvannen/Desktop/pythonProjects/twitterUsernamefromUserID/chromedriver', options=ChromeOptions)
+    # This dictionary will be populated then returned
+    DictOfUsernames = {}
+    # iterate over array of user IDs with i as ID
+    for i in user_IDs:
+        # Hardcoded URL - can it be stripped out to calling module and be passed as attr?
+        # And / or put alternative URL in here, test and leave available in comment
+        browser.get("https://twitter.com/i/user/"+i)
+        delay = 15
+        try:
+            myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'react-root')))
+            print ("Page is ready for -> ", i)
+            # Sleep for five seconds for browser to catch up
+            time.sleep(5)
+            print(browser.current_url)
+            # strip the username from the end of the URL
+            endingslash= (str(browser.current_url).split('/'))[-1]
+            # Catches cases where username doesn't form part of URL
+            if not endingslash==i:
+                DictOfUsernames[i] = endingslash
+        except TimeoutException:
+            print ("Loading took too much time!")
+
+    return(DictOfUsernames)
+
+
+returnedDict = getHandles(["80304031","25073877"])
+print(returnedDict)
