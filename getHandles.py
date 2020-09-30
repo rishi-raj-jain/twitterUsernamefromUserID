38582db9e1e4d150e93aa3fbb239bb7085f10a27
David Vannen <david@vannen.co.uk>
2020-09-30 19:32:50 +0100

    Remove getPageSource line 43 which was throwing an exception
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   getHandles.py
    
     Untracked files:
    	.DS_Store
    	__pycache__/

--- getHandles.py
+++ getHandles.py
@@ -8,32 +8,49 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
+from webdriver_manager.chrome import ChromeDriverManager
+
 def getHandles(user_IDs = None):
     ChromeOptions = webdriver.ChromeOptions()
     # Start Chrome headless so no window popping up
     ChromeOptions.add_argument("--headless")
     # Changed chrome_options to options due to deprecation
-    browser = webdriver.Chrome('./chromedrivers/chromedriver_Mac64/chromedriver', options=ChromeOptions)
+    # Setting log_level to 50 effectively silences logging as only fatal errors will be logged
+    # SO this is the install - don't / shouldn't install each time so this needs removing somehow
+    # Like first time check or take out into separate func
+    browser = webdriver.Chrome(ChromeDriverManager(log_level=50).install(), options=ChromeOptions)
     # This dictionary will be populated then returned
     DictOfUsernames = {}
-    # iterate over array of user IDs with i as ID
+    # iterate over list of user IDs with i as ID
     for i in user_IDs:
-        # Hardcoded URL - can it be stripped out to calling module and be passed as attr?
-        # And / or put alternative URL in here, test and leave available in comment
         browser.get("https://twitter.com/i/user/"+i)
         delay = 15
         try:
             myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'react-root')))
             print ("Page is ready for -> ", i)
-            # Sleep for five seconds for browser to catch up
-            time.sleep(5)
+            # Sleep for three seconds for browser to catch up
+            time.sleep(3)
             print(browser.current_url)
             # strip the username from the end of the URL
             endingslash= (str(browser.current_url).split('/'))[-1]
             # Catches cases where username doesn't form part of URL
             if not endingslash==i:
                 DictOfUsernames[i] = endingslash
+            else:
+                print("Account ", i, " Is deactivated. No username retrieved")
+                DictOfUsernames[i] = "Dead_Account"
+
+            # <Deprecated? This threw an error, the line below doesn't.> print(browser.getPageSource())
+            # print(browser.page_source)
+
+            # NOW CLOSE otherwise keeps opening windows and eventually out of memory
+            try:
+                browser.close
+                print("Closed instance")
+            except:
+                print("Couldn't close this instance")
         except TimeoutException:
             print ("Loading took too much time!")
 
     return(DictOfUsernames)
+
